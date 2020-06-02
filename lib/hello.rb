85f7d889814e39545849940b43c0ff7c9ea26a50
Lluvia Frias <lluvi.frias@gmail.com>
2020-06-02 19:37:21 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,26 @@
-def hello_t
+def hello_t(array)
+  i = 0
 
+  while i < array.length
+
+    yield array[i]
+    i += 1
+  end
+#the method is told to return the original array simply by having that array be the last line of the method
+array
 end
 
-# call your method here!
 
+
+
+
+
+
+##here we call our method with the array of names as an argument;
+##accompanying that method call with a blcok that accepts a |name| parameter
+
+# hello_t(["Tim", "Tom", "Jim"]) do |name|
+#   if name.start_with?("T")
+#     puts "Hi, #{name}"
+#   end
+# end
